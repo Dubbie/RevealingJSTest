574a227f4916f4e0680b0c75357d9954e6f0b85e
Mihó Dániel <dev.mihodaniel@gmail.com>
2017-01-31 00:08:12 +0100

    Streamlined revealing pattern

--- js/main.js
+++ js/main.js
@@ -1,34 +1,43 @@
-var listModule = (function () {
-  
-  var list = document.getElementById("list");
-  var push = document.getElementById("push");
-  var clear = document.getElementById("clear");
-  
-  
-  this.pushFunction =  function () {
-    
-  var text = document.getElementById("textbox").value;
-    if (text){
-      var nexttag = document.createElement("li");
-      var nextelement = document.createTextNode(text);
-      
-      nexttag.appendChild(nextelement);
-      list.appendChild(nexttag);
-      }
-    };
-    
-  this.clearFunction = function () {
-  
-    var listcount = list.childElementCount;
-      if (listcount > 0) {
-        list.innerHTML = "";
-      }
-    };
-  
-  this.bindUIActions = function () {
-    push.addEventListener("click", this.pushFunction);
-    clear.addEventListener("click", this.clearFunction);
-  };
+var listModule = (function() {
+  // HTML vars
+  var list        = document.getElementById("list");
+  var pushButton  = document.getElementById("push");
+  var clearButton = document.getElementById("clear");
 
-  bindUIActions();
-}) ();
+  /**
+   * Run this at the bottom to self invoke, otherwise reveal and call listModule.init() later
+   *
+   * @return {undefined}
+   */
+  function init() {
+    bindUIActions();
+  }
+
+  function pushToList() {
+    var text = document.getElementById("textbox").value;
+
+    if (text) {
+      var nextTag = document.createElement("li");
+      var nextElement = document.createTextNode(text);
+
+      nextTag.appendChild(nextElement);
+      list.appendChild(nextTag);
+    }
+  }
+
+  function clearList() {
+    var listCount = list.childElementCount;
+
+    if (listCount > 0) {
+      list.innerHTML = "";
+    }
+  }
+
+  function bindUIActions() {
+    push.addEventListener("click", pushToList);
+    clear.addEventListener("click", clearList);
+  }
+
+  // Self invoking
+  init()
+})();
